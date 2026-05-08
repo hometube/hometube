dd843ee251552566ce00ae4a0a866aebcf595525
James Hoekzema <jv.hoekzema@gmail.com>
2026-05-08 13:41:58 -0500

    update images and manifest - JH

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -21,11 +21,12 @@ export default defineConfig({
         short_name: 'HomeTube',
         theme_color: '#d30f0f',
         background_color: '#ffffff',
+        orientation: 'portrait',
         display: 'standalone',
+        display_override: ['minimal-ui'],
         icons: [
           { src: '/icon-64.png', sizes: '64x64', type: 'image/png' },
           { src: '/icon-128.png', sizes: '128x128', type: 'image/png' },
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: '/icon-256.png', sizes: '256x256', type: 'image/png' },
           { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
         ]
